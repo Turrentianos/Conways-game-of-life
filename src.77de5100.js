55f3efb45b70dc99cc8be377187d9256fe38d180
llionaki <lucianos.lionakis@ugent.be>
2020-12-28 17:29:48 +0100

    Updates

--- src.77de5100.js
+++ src.77de5100.js
@@ -41920,7 +41920,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37887" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36957" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
